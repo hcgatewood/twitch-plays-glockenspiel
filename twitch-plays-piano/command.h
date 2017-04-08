8de3a6a15df21f759be56c9f9a7cce1e006c1cbe
zrneely <zrussellneely@gmail.com>
2017-04-08 17:47:06 -0400

    Add some note operations

--- twitch-plays-piano/command.h
+++ twitch-plays-piano/command.h
@@ -1,6 +1,72 @@
 #ifndef COMMAND_H
 #define COMMAND_H
 
-// TODO class and method declarations
+#include <Arduino.h>
+
+enum class NoteName {
+  A,
+  Ais,
+  B,
+  C,
+  Cis,
+  D,
+  Dis,
+  E,
+  F,
+  Fis,
+  G,
+  Gis,
+};
+
+class Note {
+  private:
+    uint8_t _octave;
+    NoteName _name;
+
+  public:
+    Note(int octave, NoteName name) :
+      _octave(octave), _name(name) {}
+
+    // Builds a Note assuming octave 4
+    Note(NoteName name) :
+      Note(4, name) {}
+
+    // Builds the default Note, C4
+    Note() :
+      Note(NoteName::C) {}
+
+    // Disallow copying
+    void operator=(const Note& _) = delete;
+
+    // Parses a note string to a Note.
+    bool operator>>(const String& input);
+
+    bool operator>(const Note& other) const;
+    bool operator==(const Note& other) const;
+    bool operator!=(const Note& other) const;
+    bool operator<(const Note& other) const;
+
+    // Gets the address of the solenoid to activate to play this note given the lowest
+    // actuated note and assuming that all notes chromatically after it are actuated. If this
+    // note is lower than the given note, returns -1.
+    int get_output_address(const Note& bottom) const;
+
+
+    uint8_t octave() const {
+      return _octave;
+    }
+
+    uint8_t& octave() {
+      return _octave;
+    }
+
+    NoteName letter() const {
+      return _name;
+    }
+
+    NoteName& letter() {
+      return _name;
+    }
+};
 
 #endif
