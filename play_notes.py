e81bcf425da17e31a2dd46e752013fcd7fbae4bf
Hunter Gatewood <hcg@mit.edu>
2017-05-16 03:15:14 -0400

    Generalize play_notes.py

--- play_notes.py
+++ play_notes.py
@@ -8,17 +8,29 @@ Notes:
       limit and your IP gets banned for 8 hours. See
       https://help.twitch.tv/customer/portal/articles/1302780-twitch-irc.
     - Expects fields in notes.json to be filled out (pass, nick, notes).
+    - For Twitch's IRC API notes see https://dev.twitch.tv/docs/v5/guides/irc/.
     - Notes field is array of strings of form e.g. 'C# 400', with two
       space-separated fields denoting the note/chord and then the delay
       in milliseconds until the next note should play.
+
+Sample notes.json file:
+
+{
+  "pass": "oauth:<some string>",
+  "nick": "<whatever you want your nickname to be>",
+  "notes": [
+    "C5 200",
+    "D5 2000",
+    "F#6 500"
+  ]
+}
+
 """
 
 import json
 import socket
+import sys
 import time
-
-FILE = 'notes.json'
-# FILE = 'my-notes.json'
 
 TARGET = 'irc.chat.twitch.tv'
 PORT = 6667
@@ -29,7 +41,10 @@ CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
 def pass_nick_notes():
-    with open(FILE, 'r') as f:
+    if (len(sys.argv) < 1):
+        raise RuntimeError('Include the name of your notes.json file')
+    filename = sys.argv[1]
+    with open(filename, 'r') as f:
         raw_content = f.read()
         j = json.loads(raw_content)
         passw = j['pass']
@@ -50,8 +65,7 @@ def main():
     print('parsing json for pass, nick, notes')
     passw, nick, notes = pass_nick_notes()
     if passw is None or len(notes) == 0:
-        print('> FIX YOUR NOTES.JSON')
-        return
+        raise RuntimeError('Check play_notes.py for how to format your notes.json file')
     print('\t> found pass:', passw)
     print('\t> found nick:', nick)
     print('\t> found notes:', notes)
